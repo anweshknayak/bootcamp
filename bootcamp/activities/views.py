61fda2429cba5de2a202a5c06300e2af20dd977e
anweshknayak <anweshknayak@gmail.com>
2017-01-15 23:31:42 +0530

    CBV

--- bootcamp/activities/views.py
+++ bootcamp/activities/views.py
@@ -1,43 +1,46 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
 from bootcamp.activities.models import Notification
 from bootcamp.decorators import ajax_required
 
 
 @login_required
-def notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(to_user=user)
-    unread = Notification.objects.filter(to_user=user, is_read=False)
-    for notification in unread:
-        notification.is_read = True
-        notification.save()
+class Notifications(View):
+    def get(self, request):
+        user = request.user
+        notifications = Notification.objects.filter(to_user=user)
+        unread = Notification.objects.filter(to_user=user, is_read=False)
+        for notification in unread:
+            notification.is_read = True
+            notification.save()
 
-    return render(request, 'activities/notifications.html',
-                  {'notifications': notifications})
-
-
-@login_required
-@ajax_required
-def last_notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(to_user=user,
-                                                is_read=False)[:5]
-    for notification in notifications:
-        notification.is_read = True
-        notification.save()
-
-    return render(request,
-                  'activities/last_notifications.html',
-                  {'notifications': notifications})
+        return render(request, 'activities/notifications.html',
+                      {'notifications': notifications})
 
 
 @login_required
 @ajax_required
-def check_notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(to_user=user,
+class Last_notifications(View):
+    def get(self, request):
+        user = request.user
+        notifications = Notification.objects.filter(to_user=user,
                                                 is_read=False)[:5]
-    return HttpResponse(len(notifications))
+        for notification in notifications:
+            notification.is_read = True
+            notification.save()
+
+        return render(request, 'activities/last_notifications.html',
+                       {'notifications': notifications})
+
+
+@login_required
+@ajax_required
+class Check_notifications(request):
+    def get(self, request):
+        user = request.user
+        notifications = Notification.objects.filter(to_user=user,
+                                                is_read=False)[:5]
+        return HttpResponse(len(notifications))
